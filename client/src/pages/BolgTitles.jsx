c21ba7468650d69d40ef8e0d0e8548e04a5c8de5
Junaid Khan <pathanjunaid7888@gmail.com>
2025-08-25 17:02:00 +0530

    Added Navbar

---
+++ client/src/pages/BolgTitles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BolgTitles = () => {
+  return (
+    <div>BolgTitles</div>
+  )
+}
+
+export default BolgTitles
---
+++ client/src/pages/RemoveBackground.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RemoveBackground = () => {
+  return (
+    <div>RemovedBackground</div>
+  )
+}
+
+export default RemoveBackground
