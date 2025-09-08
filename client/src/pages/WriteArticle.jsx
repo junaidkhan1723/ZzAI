7dcdfdca75e6992b05bc1eab17a646e63fcaa49b
Junaid Khan <pathanjunaid7888@gmail.com>
2025-09-08 16:16:39 +0530

    added right col

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -9,16 +9,17 @@ const WriteArticle = () => {
   ];
 
   const [selectedLength, setSelectedLength] = useState(articleLength[0]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
-  const onSubmitHandler = async (e)=>{
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
   };
 
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       {/** left col */}
-      <form onSubmit={onSubmitHandler}
+      <form
+        onSubmit={onSubmitHandler}
         action=""
         className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
       >
@@ -28,7 +29,9 @@ const WriteArticle = () => {
         </div>
         <p className="mt-6 text-sm font-medium">Article Topic</p>
 
-        <input onChange={(e)=> setInput(e.target.value)} value={input}
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
           type="text"
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
           placeholder="The Future Of artificial intelligence is..."
@@ -39,11 +42,15 @@ const WriteArticle = () => {
 
         <div className="mt-3 gap-3 flex flex-wrap sm:max-w-9/11">
           {articleLength.map((item, index) => (
-            <span onClick={()=> setSelectedLength(item)}
+            <span
+              onClick={() => setSelectedLength(item)}
               key={index}
               className={`text-xs px-4 py-1 border rounded-full cursor-pointer 
-                ${selectedLength.text === item.text ? 'bg-blue-50 text-blue-700'
-              : 'text-gray-500 border-gray-300'} `}
+                ${
+                  selectedLength.text === item.text
+                    ? "bg-blue-50 text-blue-700"
+                    : "text-gray-500 border-gray-300"
+                } `}
             >
               {item.text}
             </span>
@@ -51,15 +58,31 @@ const WriteArticle = () => {
         </div>
         <br />
 
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r
-        from-blue-600 to-indigo-500 text-white px-4 p-2 mt-6 text-sm rounded-lg cursor-pointer transition-all "> 
-          <Edit className="w-5"/>
+        <button
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r
+        from-blue-600 to-indigo-500 text-white px-4 p-2 mt-6 text-sm rounded-lg cursor-pointer transition-all "
+        >
+          <Edit className="w-5" />
           Generate Article
         </button>
       </form>
       {/** right col */}
 
-      <div className=""></div>
+      <div className="w-full max-w-lg p-4  bg-white rounded-lg flex flex-col border
+      border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3">
+          <Edit className="w-5 h-5 text-blue-600"/>
+          <h1 className="text-xl font-semibold">Generated article</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+                       <Edit className="w-9 h-9"/>
+                      <p>Enter a topic and click "Generate article" to get started</p>
+ 
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
