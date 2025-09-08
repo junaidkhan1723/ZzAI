3629afa4f032e0839bae9037f1de0a176ecec2cb
Junaid Khan <pathanjunaid7888@gmail.com>
2025-09-08 16:00:27 +0530

    updated form

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Sparkles } from "lucide-react";
+import { Edit, Sparkles } from "lucide-react";
 
 const WriteArticle = () => {
   const articleLength = [
@@ -9,11 +9,16 @@ const WriteArticle = () => {
   ];
 
   const [selectedLength, setSelectedLength] = useState(articleLength[0]);
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault();
+  };
+
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       {/** left col */}
-      <form
+      <form onSubmit={onSubmitHandler}
         action=""
         className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
       >
@@ -23,7 +28,7 @@ const WriteArticle = () => {
         </div>
         <p className="mt-6 text-sm font-medium">Article Topic</p>
 
-        <input
+        <input onChange={(e)=> setInput(e.target.value)} value={input}
           type="text"
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
           placeholder="The Future Of artificial intelligence is..."
@@ -44,6 +49,13 @@ const WriteArticle = () => {
             </span>
           ))}
         </div>
+        <br />
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r
+        from-blue-600 to-indigo-500 text-white px-4 p-2 mt-6 text-sm rounded-lg cursor-pointer transition-all "> 
+          <Edit className="w-5"/>
+          Generate Article
+        </button>
       </form>
       {/** right col */}
 
