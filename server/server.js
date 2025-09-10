8907d40c29a5fd777333a2228cbb1f326d17beae
Junaid Khan <pathanjunaid7888@gmail.com>
2025-09-10 17:54:25 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { clerkMiddleware, requireAuth } from '@clerk/express';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 const app = express();
 
@@ -9,14 +9,12 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
-app.get('/', (req, res)=> res.send('server is live!'));
+app.get("/", (req, res) => res.send("server is live!"));
 
 app.use(requireAuth());
 
-
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log('Server is running on port', PORT)
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
 });
