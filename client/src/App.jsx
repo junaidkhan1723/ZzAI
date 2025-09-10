c73d509899b636b80a369f35dffba9c48d9f8e38
Junaid Khan <pathanjunaid7888@gmail.com>
2025-09-10 18:42:15 +0530

    Add getToken logging with useAuth in useEffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,42 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Layout from './pages/Layout'
-import Dashboard from './pages/Dashboard'
-import WriteArticle from './pages/WriteArticle'
-import BolgTitles from './pages/BolgTitles'
-import GenerateImages from './pages/GenerateImages'
-import RemoveBackground from './pages/RemoveBackground'
-import RemoveObject from './pages/RemoveObject'
-import ReviewResume from './pages/ReviewResume'
-import Community from './pages/Community'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import WriteArticle from "./pages/WriteArticle";
+import BolgTitles from "./pages/BolgTitles";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 function App() {
-
-
+  const { getToken } = useAuth();
+  useEffect(() => {
+    getToken().then((token) => console.log(token));
+  }, []);
   return (
     <>
-    <div>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/ai' element={<Layout/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path='write-article' element={<WriteArticle/>}/>
-          <Route path='blog-titles' element={<BolgTitles/>}/>
-          <Route path='generate-images' element={<GenerateImages/>}/>
-          <Route path='remove-background' element={<RemoveBackground/>}/>
-          <Route path='remove-object' element={<RemoveObject/>}/>
-          <Route path='review-resume' element={<ReviewResume/>}/>
-          <Route path='community' element={<Community/>}/>
-        </Route>
-      </Routes>
-    </div>
-        
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ai" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="write-article" element={<WriteArticle />} />
+            <Route path="blog-titles" element={<BolgTitles />} />
+            <Route path="generate-images" element={<GenerateImages />} />
+            <Route path="remove-background" element={<RemoveBackground />} />
+            <Route path="remove-object" element={<RemoveObject />} />
+            <Route path="review-resume" element={<ReviewResume />} />
+            <Route path="community" element={<Community />} />
+          </Route>
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
