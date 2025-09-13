399dc96be3882b4033873b6070be57223fc54eb4
Junaid Khan <pathanjunaid7888@gmail.com>
2025-09-13 13:17:23 +0530

    created userRoutes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  getPublishCreations,
+  getUserCreations,
+  toggleLikeCreation,
+} from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get("/get-user-creations", auth, getUserCreations);
+userRouter.get("/get-published-creations", auth, getPublishCreations);
+userRouter.post("/toggle-like-creation", auth, toggleLikeCreation);
+
+export default userRouter;
